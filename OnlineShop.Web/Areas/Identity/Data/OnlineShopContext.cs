e6f248ddb5c5caccd0f61f847360c68996ab86ec
Ehsan Elhampour <ehsan.elhampour@gmail.com>
2021-09-05 12:24:17 +0430

    add dbcontext

---
+++ OnlineShop.Web/Areas/Identity/Data/OnlineShopContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Web.Areas.Identity.Data;
+
+namespace OnlineShop.Web.Data
+{
+    public class OnlineShopContext : IdentityDbContext<OnlineShopUser>
+    {
+        public OnlineShopContext(DbContextOptions<OnlineShopContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ OnlineShop.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineShop.Web.Areas.Identity.Data;
+using OnlineShop.Web.Data;
+
+[assembly: HostingStartup(typeof(OnlineShop.Web.Areas.Identity.IdentityHostingStartup))]
+namespace OnlineShop.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OnlineShopContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("OnlineShopContextConnection")));
+
+                services.AddDefaultIdentity<OnlineShopUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<OnlineShopContext>();
+            });
+        }
+    }
+}
